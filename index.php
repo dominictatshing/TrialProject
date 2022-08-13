5a44d86fd4ca764c56bd95982ee7629e3b911a35
Dominic Tang <dominictatshing@gmail.com>
2022-08-13 20:04:27 +0800

    new commit after revert good

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Jquery 3.6.0 -->
+        <script type="text/javascript" src="jquery-3.6.0.js"></script>
+    </head>
+    <body>
+        <div>hello, welcome here!</div>
+
+        <form action="" method="GET">
+            <input  name="email" type="text" value=""/>
+            <input type="submit" value="Submit"/>
+        </form> 
+    </body>
+
+    <h2>I am new commit</h2>
+
+    <script type="text/javascript">
+
+        //function to retrieve url query string's key value
+        var getUrlParameter = function (sParam) {
+            
+            var sPageURL = window.location.search.substring(1),
+                sURLVariables = sPageURL.split('&'),
+                sParameterName,
+                i;
+
+            for (i = 0; i < sURLVariables.length; i++) {
+                sParameterName = sURLVariables[i].split('=');
+
+                if (sParameterName[0] === sParam) {
+                    return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+                }
+            }
+
+            return false;
+        };
+
+        var email = getUrlParameter('email');
+
+        console.log("email: " + email);
+
+        $("form input[type=text]").val(email);
+
+    </script>
+
+    
+
+</html>
